5730311e4e56585579d924156b4629d53c55820f
Arcanjo Cassiel <arcanjocassiel687@gmail.com>
2020-10-15 22:21:37 -0300

    create server and init back-end

--- public/scripts/orphanages.js
+++ public/scripts/orphanages.js
@@ -6,22 +6,36 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
 
 // create icon 
 const icon = L.icon({
-        iconUrl: "./public/images/map-marker.svg",
-        iconSize: [58, 68],
-        iconAnchor: [29, 68],
-        popupAnchor: [170, 2]
-    })
-    // create popup overlay
-const popup = L.popup({
-    closeButton: false,
-    className: 'map - popup',
-    minWidth: 240,
-    minHeight: 240,
-}).setContent(`Lar da meninas  <a   href="orphanage.html?id=1"
-  class="choose-orphanege" ><img src="./public/images/arrow-white.svg"></a> `)
+    iconUrl: "/images/map-marker.svg",
+    iconSize: [58, 68],
+    iconAnchor: [29, 68],
+    popupAnchor: [170, 2]
+})
 
-// create and  marker
-L
-    .marker([-9.0716449, -44.3520314], { icon })
-    .addTo(map)
-    .bindPopup(popup)
+function addMarker({ id, name, lat, lng }) {
+
+    // create popup overlay
+    const popup = L.popup({
+        closeButton: false,
+        className: 'map - popup',
+        minWidth: 240,
+        minHeight: 240,
+    }).setContent(`${name} <a   href="/orphanage?id=${id}" class="choose-orphanege" ><img src="/images/arrow-white.svg"></a> `)
+
+    // create and  marker
+    L
+        .marker([lat, lng], { icon })
+        .addTo(map)
+        .bindPopup(popup)
+}
+
+const orphanagsSpan = document.querySelectorAll('.orphanages span')
+orphanagsSpan.forEach(span => {
+    const orphanage = {
+        id: span.dataset.id,
+        name: span.dataset.name,
+        lat: span.dataset.lat,
+        lng: span.dataset.lng,
+    }
+    addMarker(orphanage)
+})
